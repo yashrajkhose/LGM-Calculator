f66d1cc1510a5f1d83891b40180cf4c5065e4cdc
yashrajkhose <79778850+yashrajkhose@users.noreply.github.com>
2022-04-16 10:58:22 +0530

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,61 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+           
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
+// let display = document.getElementById('display');
+
+// let buttons = Array.from(document.getElementsByClassName('button'));
+
+// buttons.map( button => {
+//     button.addEventListener('click', (e) => {
+//         switch(e.target.innerText){
+//             case 'C':
+//                 display.innerText = '';
+//                 break;
+//             case '=':
+//                 try{
+//                     display.innerText = eval(display.innerText);
+//                 } catch {
+//                     display.innerText = "Error"
+//                 }
+//                 break;
+//             case case '←':
+                // if (display.innerText){
+                //     display.innerText = display.innerText.slice(0, -1);
+                //  }:
+//                 if (display.innerText){
+//                    display.innerText = display.innerText.slice(0, -1);
+//                 }
+//                 break;
+//             default:
+//                 display.innerText += e.target.innerText;
+//         }
+//     });
+// });
+
+
