c1d2135dfc72c8b573d5c2bba1ad71033ddd4038
yashrajkhose <79778850+yashrajkhose@users.noreply.github.com>
2022-04-16 12:05:33 +0530

    Update app.js

--- calculator/app.js
+++ calculator/app.js
@@ -27,35 +27,4 @@ for (item of buttons) {
     })
 }
 
-// let display = document.getElementById('display');
-
-// let buttons = Array.from(document.getElementsByClassName('button'));
-
-// buttons.map( button => {
-//     button.addEventListener('click', (e) => {
-//         switch(e.target.innerText){
-//             case 'C':
-//                 display.innerText = '';
-//                 break;
-//             case '=':
-//                 try{
-//                     display.innerText = eval(display.innerText);
-//                 } catch {
-//                     display.innerText = "Error"
-//                 }
-//                 break;
-//             case case '←':
-                // if (display.innerText){
-                //     display.innerText = display.innerText.slice(0, -1);
-                //  }:
-//                 if (display.innerText){
-//                    display.innerText = display.innerText.slice(0, -1);
-//                 }
-//                 break;
-//             default:
-//                 display.innerText += e.target.innerText;
-//         }
-//     });
-// });
-
 
